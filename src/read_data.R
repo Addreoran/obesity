5db83a61ce117261648c7ad6aa518fb5899b5612
Joanna <jm.ziemska@gmail.com>
2026-02-25 15:02:10 +0100

    Update read_data.R

--- src/read_data.R
+++ src/read_data.R
@@ -3,10 +3,19 @@
 library(phyloseq)
 
 
-read_and_parse_data<-funtion(metadata_path, otu_path, tax_path){
-  metadata <- read.csv(metadata_path)
-  otu <- read.csv(otu_path)
-  tax <- read.csv(tax_path)
-  ps<-phyloseq(otu, metadatam tax)
-  return(ps)
+read_and_parse_data <- function(metadata_path, otu_path, tax_path) {
+  
+  otu <- read.csv(otu_path, row.names = 1) 
+  otu <- otu_table(as.matrix(otu), taxa_are_rows = TRUE) 
+  colnames(otu)<-sub("^X", "", colnames(otu))
+  
+  metadata <- read.csv(metadata_path, row.names = 1)
+  metadata <- sample_data(metadata) 
+  rownames(metadata)<-metadata$id
+  
+  tax <- read.csv(tax_path, row.names = 1) 
+  tax <- tax_table(as.matrix(tax)) 
+  
+  ps <- phyloseq(otu, metadata, tax) 
+  return(ps) 
 }
