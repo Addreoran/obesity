227796a2a041c8f879c960e16f0ecaf55438ada3
Joanna <jm.ziemska@gmail.com>
2026-02-27 12:59:28 +0100

    Update AnosimPermanova.R

--- src/AnosimPermanova.R
+++ src/AnosimPermanova.R
@@ -6,9 +6,6 @@ library(devtools)
 library(pairwiseAdonis)
 
 Permanova <-function(ps, method){
-  data_prop_bray <- phyloseq::distance(ps, method = method)
-  sampledf <- data.frame(sample_data(ps))
-  ht_well <- adonis2(data_prop_bray ~ research, data = sampledf)
   ps_dist_matrix <- phyloseq::distance(ps, method=method)
   permanova_pairwise <- vegan::adonis2(ps_dist_matrix ~ phyloseq::sample_data(ps)$research, permutations = 9999)
   betadisper(ps_dist_matrix, sample_data(ps)$research)
@@ -19,6 +16,29 @@ Permanova <-function(ps, method){
 Anosim <- function(ps, method){
   metadata <- data.frame(sample_data(ps))
   anosim_test<-anosim(phyloseq::distance(ps, method=method), 
+                             metadata$research, permutations = 9999)
+  return(c(anosim_test$signif, anosim_test$statistic))
+}
+
+
+PermanovaCLR <-function(ps, method){
+  physeq_clr <- microbiome::transform(ps, "clr")
+  otu_clr <- as.data.frame(otu_table(physeq_clr))
+  dist_matrix <- vegan::vegdist(otu_clr, method = method)
+
+  
+  permanova_pairwise <- vegan::adonis2(dist_matrix ~ phyloseq::sample_data(ps)$research, permutations = 9999)
+  betadisper(ps_dist_matrix, sample_data(ps)$research)
+  return(c(permanova_pairwise$`Pr(>F)`[1], permanova_pairwise$R2[1]))
+}
+
+
+AnosimCLR <- function(ps, method){
+  metadata <- data.frame(sample_data(ps))
+  physeq_clr <- microbiome::transform(ps, "clr")
+  otu_clr <- as.data.frame(otu_table(physeq_clr))
+  dist_matrix <- vegan::vegdist(otu_clr, method = "euclidean")
+  anosim_test<-anosim(dist_matrix, 
                              metadata$research, permutations = 9999)
   return(c(anosim_test$signif, anosim_test$statistic))
 }
