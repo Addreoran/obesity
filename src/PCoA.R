ede51f64e571650dbf46d365fbb85cee7eb8849b
Joanna <jm.ziemska@gmail.com>
2026-04-02 10:27:55 +0200

    Update PCoA.R

--- src/PCoA.R
+++ src/PCoA.R
@@ -2,14 +2,28 @@
 #BiocManager::install("microbiome")
 library(microbiome)
 
-PCoAAitch <-function(ps, save_path, width=10, height=8){
+PCoAAitch <-function(ps_genus, save_path, width=10, height=8){
   #ps <- transform_sample_counts(ps, function(x) x + 1)
-  ps.clr <- microbiome::transform(ps, "clr")
+  ps.clr <- microbiome::transform(ps_genus, "clr")
   pcoa_euclidean <- ordinate(ps.clr, "PCoA", "euclidean")
   image <- phyloseq::plot_ordination(ps.clr, pcoa_euclidean, color = "research") +
     geom_point(size = 3) +
     geom_text(aes(label = sample_data(ps.clr)$CAP), vjust = -0.5) +
-    geom_polygon(stat = "ellipse", aes(fill = research), alpha = 0.3)
+    geom_polygon(stat = "ellipse", aes(fill = research), alpha = 0.3) +
+  scale_color_manual(
+    values = c(
+      "Controls" = "#7a7a7a",
+      "Overweight/Obesity" = "#FF0000", 
+            "cap>=250" = "#7a7a7a", 
+    "cap<250" = "#FF0000"
+    )
+  )+
+  scale_fill_manual(    values = c(
+      "Controls" = "#7a7a7a",
+      "Overweight/Obesity" = "#FF0000", 
+      "cap>=250" = "#7a7a7a", 
+    "cap<250" = "#FF0000"
+    ))
   ggsave(file=save_path, plot=image, width=10, height=8)
 }
 
